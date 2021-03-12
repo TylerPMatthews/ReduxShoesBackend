c3deb83de6e4ccc6969103a841c9e5dd5a0456d8
ty <tylerpmatthews1998@gmail.com>
2021-03-12 01:26:45 -0500

    Base server setup

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
-module.exports = server
+server.get("/", (req, res) => {
+  res.json({ API: "Online" });
+});
+
+module.exports = server;
